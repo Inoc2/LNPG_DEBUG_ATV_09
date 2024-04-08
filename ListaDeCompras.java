41545195b6f7c8e686833972e0d2b0b20ef5d9f7
Alvacy Gonçalves <138629360+Inoc2@users.noreply.github.com>
2024-04-08 13:57:15 -0300

    Add files via upload

---
+++ ListaDeCompras.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class ListaDeCompras {
+    public static void main(String[] args) {
+        ListaDeCompras lista = new ListaDeCompras();
+        lista.adicionarItem("Maçã");
+        lista.adicionarItem("Banana");
+        lista.adicionarItem("Pêra");
+        lista.removerItem("Banana");
+        lista.exibirLista();
+    }
+
+    private ArrayList<String> itens;
+
+    public ListaDeCompras() {
+        this.itens = new ArrayList<>();
+    }
+
+    public void adicionarItem(String item) {
+        itens.add(item);
+    }
+
+    public void removerItem(String item) {
+        itens.remove(item); 
+    }
+
+    public void exibirLista() {
+        System.out.println("Lista de Compras:");
+        for (String item : itens) {
+            System.out.println("- " + item);
+        }
+    }
+}
